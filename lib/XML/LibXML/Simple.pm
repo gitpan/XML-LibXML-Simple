577d4f47b3ce8c5e7b9e6fa46627704ff559b29e
Mark Overmeer <mark@overmeer.net>
2013-03-02 11:00:49 +0000

    Import of MARKOV/XML-LibXML-Simple-0.93 from CPAN.
    gitpan-cpan-distribution: XML-LibXML-Simple
    gitpan-cpan-version:      0.93
    gitpan-cpan-path:         MARKOV/XML-LibXML-Simple-0.93.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/XML/LibXML/Simple.pm
+++ lib/XML/LibXML/Simple.pm
@@ -1,10 +1,10 @@
-# Copyrights 2008-2012 by [Mark Overmeer].
+# Copyrights 2008-2013 by [Mark Overmeer].
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 2.00.
+# Pod stripped from pm file by OODoc 2.01.
 package XML::LibXML::Simple;
 use vars '$VERSION';
-$VERSION = '0.92';
+$VERSION = '0.93';
 
 use base 'Exporter';
 
